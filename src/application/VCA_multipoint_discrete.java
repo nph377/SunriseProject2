c0e42bdfe13251e99ba6bc3ba16ef72c3dd59823
nph377 <nph377@ME-D22458.austin.utexas.edu>
2025-07-03 10:54:44 -0500

    --- automatic rebase to remote; old local SHA: 95cccedffcf380d11da453f3a62badcfce60b2a4 ---

--- src/application/VCA_multipoint_discrete.java
+++ src/application/VCA_multipoint_discrete.java
@@ -104,7 +104,7 @@ public class VCA_multipoint_discrete extends RoboticsAPIApplication {
 		}
 		else if (response == 1) {
 			logger.info("orienting TCP");
-			Frame v = new Frame(x,y,z,0,0,0);
+			Frame v = new Frame(x,y,z,a,0,c);
 			robot.getFlange().move(lin(v).setJointVelocityRel(.2));
 			logger.info("TCP orientation complete");
 		}
