a8309337560b676108046154c3db6947d67e9bfa
nph377 <nph377@ME-D22458.austin.utexas.edu>
2025-07-08 16:11:08 -0500

    --- automatic rebase to remote; old local SHA: ff521563b1355d34f23678b54d426b58accefa5b ---

--- src/application/encoder_test.java
+++ src/application/encoder_test.java
@@ -56,7 +56,7 @@ public class encoder_test extends RoboticsAPIApplication {
 			}
 			flange.setLEDBlue(false);
 			try {
-				Thread.sleep(100);
+				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
