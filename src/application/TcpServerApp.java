cb44449eb856ec692778d212a393bf29420a6545
MINIMAX Lab <MINIMAX Lab@DESKTOP-M4MEDN7>
2025-07-11 15:01:36 -0500

    --- automatic rebase to remote; old local SHA: bc6d07f8258b47d56b4884d7d9466390c534b6d1 ---

--- src/application/TcpServerApp.java
+++ src/application/TcpServerApp.java
@@ -28,7 +28,7 @@ public class TcpServerApp extends RoboticsAPIApplication {
             reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
             String line;
-            while ((line = reader.readLine()) != null && ((line = reader.readLine()) !="exit") ) {
+            while ((line = reader.readLine()) != null) {
                 getLogger().info("Received: " + line);
                 // Add robot logic here
                 int value=Integer.parseInt(line);
